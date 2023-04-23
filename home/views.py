837e8f3c3bead333839a230d3a5e8eb45d9bb552
Matt Murphy <mattmurph888@gmail.com>
2023-04-23 11:34:40 -0400

    registration and entitity relationship diagram

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     context = {}
@@ -27,3 +28,22 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            context = {'form': form}
+            return render(request, 'register.html', context)
+    else:
+        form = SignUpForm()
+        context = {'form': form}
+        return render(request, 'register.html', context)
