8662c9bb6250b95317c788506467c7924819f44f
Matt Murphy <mattmurph888@gmail.com>
2023-04-25 10:29:45 -0400

    database work, added levels and user_level_records, and connections between them

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Level, User_Level_Record
 
 # Register your models here.
+admin.site.register(Level)
+admin.site.register(User_Level_Record)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Level(models.Model):
+    level_number = models.IntegerField(unique=True, default=None)
+    level_title = models.CharField(max_length=50)
+    level_text = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f'Level Title: {self.level_title}'
+    
+
+class User_Level_Record(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, default=None)
+    accuracy = models.IntegerField(default=0)
+    speed = models.IntegerField(default=0)
+    focus = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}'s {self.level.level_title} record"
